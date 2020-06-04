49c887e55ea22cd7ece7adba2ed86cd8bca76871
sidiel8 <38636460+sidiel8@users.noreply.github.com>
2020-06-04 12:49:25 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,104 +1,216 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\User ;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-if(Auth::check()){
-        return redirect('home');
-    }
+<template>
+    <div>
+<div class="card text-center">
+ 
+  <div class="card-body">
+    <h4 class="card-title">OPERATIONS </h4>
     
-    else{
-        return redirect('login') ;
-    }
-*/
-Route::get('/login', 'Log@showLoginForm')->name('login');
-Route::post('/login', 'Log@login');
-Route::post('/logout', 'Log@logout');
-//Route::get('/dynamic_pdf/portes', 'DynamicPDFController@portes');
-//Route::get('/dynamic_pdf/autres_mesures', 'DynamicPDFController@autres_mesures');
-//Auth::routes();
-Route::get('/dynamic_pdf/pvc/pdf', 'RecPDFController@pdf_pvc');
-Route::get('/dynamic_pdf/bois/pdf', 'RecPDFController@pdf_bois');
-
-Route::get('/dynamic_pdf/portes/pdf', 'DynamicPDFController@pdf_portes');
-Route::get('/dynamic_pdf/autres_mesures/pdf', 'DynamicPDFController@pdf_mesures');
-
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/clienttoedit/{id}', 'HomeController@clientToEdit')->name('can:mange');
-Route::get('/deleteclient/{id}', 'HomeController@deleteClient')->name('can:mange');
-Route::get('/users/input', 'HomeController@allUsers')->middleware('auth') ;
-Route::get('/clients' , 'ClientController@index')->name('new-client') ;
-Route::get('/clients/edit/{id}' , 'ClientController@edit')->middleware('auth') ;
-Route::patch('/clients/edit/{id}' , 'ClientController@update')->middleware('auth') ;
-Route::patch('/clients/editstaut/{id}' , 'ClientController@statu')->middleware('auth') ;
-
-Route::post('/client' , 'ClientController@store') ;
-
-Route::post('/confirmation' , 'ConfirmationController@store')->middleware(['can:mange'||'can:actiu']) ;
-Route::get('/confirmation/{id}' , 'ConfirmationController@index')->middleware(['can:mange'||'can:actiu']) ;
-
-Route::get('/devis/client/{id}' , 'DevisController@index')->middleware('auth') ;
-Route::post('/devis' , 'DevisController@store')->middleware('auth') ;
-Route::get('/devis' , 'DevisController@tousLesDevis')->middleware('can:mange');
-Route::get('/devis/fac' , 'DevisController@tousLesFac')->middleware('can:mange');
-Route::delete('/devis/{id}' , 'DevisController@delete')->middleware('can:mange') ;
-Route::patch('devis/fact/{id}','DevisController@addFacture')->middleware('auth');
-Route::patch('devis/edit/{id}','DevisController@editDevis')->middleware('auth');
+  <div class="row">
+ 
+     <div class="col-md-4" >   
+    <form  @submit="recherche" >
+          <b style="float:left">Recherche Des Client</b><br>
+        <hr>
+      
+    <div class="form-group">
+    <input type="number"  min="0"  class="form-control"  placeholder="N° Telephone"  v-model="phone" required>
+    </div>
     
-Route::get('/facture/client/{id}' , 'FactureController@index')->middleware('can:mange') ;
-Route::post('/facture' , 'FactureController@store')->middleware('can:mange') ;
-Route::get('/factures' , 'FactureController@tousLesFactures')->middleware('can:mange') ;
-Route::delete('/facture/{id}' , 'FactureController@delete')->middleware('can:mange') ;
-Route::get('/facture/{id}' , 'FactureController@get')->middleware('can:mange') ;
-Route::patch('/facture/add/{id}' , 'FactureController@ajouter')->middleware('can:mange') ;
-Route::patch('/facture/remove/{id}' , 'FactureController@retirer')->middleware('can:mange') ;
-
-Route::get('/mesures' , 'MesureController@tousLesMesures')->middleware('can:mange') ;
-Route::get('/mesuresactiu' , 'MesureController@tousLesMesuresActiu')->middleware('can:actiu') ;
-Route::get('/mesures/client/{id}' , 'MesureController@index')->middleware('auth');
-Route::post('/mesure' , 'MesureController@store')->middleware('auth');
-Route::delete('/mesure/{id}' , 'MesureController@delete')->middleware(['can:mange'||'can:actiu']) ;
 
 
-Route::post('/croquis' , 'CroquisController@store')->middleware(['can:mange'||'can:actiu']);
-Route::get('/croquis/client/{id}' , 'CroquisController@index')->middleware('auth');
-Route::post('/croquis/image' , 'CroquisController@upload')->middleware(['can:mange'||'can:actiu']);
-
-Route::post('/reclamations' , 'ReclamationController@store')->middleware('can:mange') ;
-Route::get('/reclamations' , 'ReclamationController@index')->middleware('can:mange') ;
-Route::patch('/reclamation/edit/{id}' , 'ReclamationController@update')->middleware('can:mange') ;
-Route::patch('/reclamation/cancel/{id}' , 'ReclamationController@cancel')->middleware('can:mange') ;
-Route::delete('/reclamation/{id}' , 'ReclamationController@delete')->middleware('can:mange') ;
-
-Route::get('/decharges' , 'DechargeController@index')->middleware('can:mange') ;
-Route::post('/decharge' , 'DechargeController@store')->middleware('can:mange') ;
+     
+    <div class="form-group  ">
+    <button  class="btn btn-success btn-block "><span > OK</span><span></span></button>
+    </div>
+      
+        </form>  
+     </div> 
 
 
-Route::get('/files/{type}/{id?}', 'FileController@index');
+     <div  class="col-md-4">   
+    <form  @submit="print" >
+          <b style="float:left">Rapports</b><br>
+               <hr>
+      
+    <div class="form-group ">
+    <b for="start" style="float:left">Date Debut:</b>
 
-Route::post('files/add', 'FileController@store');
-Route::post('files/edit/{id}', 'FileController@edit');
-Route::post('files/delete/{id}', 'FileController@destroy');
+<input type="date" id="start" v-model="start"
+       value="2020-05-28"
+       min="2020-05-28" max="2020-12-31">
+    </div>
+
+     <div class="form-group "  >
+    <b for="start" style="float:left">Date Fin:</b>
+
+<input type="date" id="end"  v-model="end"
+       value="2020-05-28"
+       min="2020-05-28" max="2020-12-31">
+    </div>
+    
 
 
-/*  admins stuff */ 
-  //Route::resource('/admin', 'Admin\\UsersController');
+     
+    <div class="form-group  ">
+    <button  class="btn btn-success btn-block "><span > Print </span><span></span></button>
+    
+    </div>  
+        </form>  
+     </div> 
+
+  </div>
+
+</div>
+
+<ul class="list-group" >
+   <div v-if="loading">
+     <i class="fas fa-spinner fa-spin"></i>
+   </div>
+   <div v-else>
+         <div v-if="clients.length==0">
+    <span class="alert alert-danger "> {{resultat}} </span>
+   </div>
+ <div v-else>
+     <li :id="client.id"   v-for="client in clients"  :key="client.id"  class="list-group-item d-flex justify-content-between align-items-center">
+   {{client.name}}
+    <span  @click="suppClient(client.id)" style="color:red ">Supprimer</span>
+    
+  </li>
+ <div class="card-body"  style="border:1px solid black">
+  <nav aria-label="">
+  <ol class="breadcrumb" dir="rtl">
+    <li class="breadcrumb-item active" dir="rtl"><h3>تقرير</h3></li>
+  </ol>
+</nav>
+         
+
+       <p style="font-size: 18px">   يحتوي النقرير التالي علي المعطيات بين تاريخي </p>
+            <p style="font-size: 18px" dir="rtl"> {{this.start}}   الى  {{this.end}}  </p>
+
+    <tr v-for="client in clts" :key="client.id">
+      <td >{{client.name}}</td>
+      <td>{{client.numero}}</td>
+      <td>{{client.email ?  client.email :  '--------------' }}</td>
+      <td>{{client.vendeur}}</td>
+      <td>{{client.quartier}}</td>
+      <td>{{
+           client.created_at
+        }}</td>
+     
+     
+    </tr>
+</div>
+</div>
+
   
-  Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:mange')->group(function(){
-    Route::resource('/', 'UsersController');
-    Route::get('/users/{id}', 'UsersController@edit')->name('edit');
-    Route::post('/users/{id}', 'UsersController@update')->name('update');
-    //Route::get('/clients', 'UsersController@showClients')->name('clients');
-    //Route::get('/users', 'UsersController@showUsers')->name('users');
-   
-    Route::get('/clients/{id}', 'UsersController@showClient')->name('client');
-  }) ;
 
+
+
+
+   </div>
+ 
+</ul>
+    </div>    
+  </div> 
+</template>
+<script>
+
+export default {
+
+      data () {
+    return {
+      clients:{} ,
+      loading: false,
+      errored: false ,
+      phone:'',
+      resultat:'Ce Client N\'existe Pas ',
+      start:'',
+      end:'',
+      clts:{}
+     
+    }
+      },
+
+    methods: {
+    suppClient($id){
+          
+this.$dialog.confirm("êtes-vous sûr de vouloir supprimer ce client ? ", {
+        
+         })
+	       .then((dialog) => {
+            axios
+      .get('/deleteclient/'+$id)
+      .then(response => {
+        console.log(response);
+         $('#'+$id).hide(500);
+        //this.del = false 
+       
+      
+      }).finally(() => {}
+       
+        )
+
+
+           })
+    .catch(() => {
+        // Triggered when cancel button is clicked
+
+        console.log('Delete aborted');
+    });
+    },
+
+
+
+
+
+    recherche(e){
+         
+                this.loading= true ;
+                e.disabled;
+        axios.get('/clienttoedit/'+this.phone, {
+                   
+               }).then(response => this.clients=response.data
+
+            
+               
+               
+                ).catch(error => {
+        console.log(error)
+            
+      })
+      .finally(() => this.loading = false)
+
+    },
+
+    print(e){
+         
+               // this.loading= true ;
+                e.disabled;
+        axios.post('/print/', {
+                     start:this.start,
+                     end:this.end ,
+               }).then(response => this.clts=response.data
+
+            
+               
+               
+                ).catch(error => {
+        console.log(error)
+            
+      })
+      .finally(() => this.loading = false)
+
+    }
+
+
+    }
+
+
+
+
+
+
+      }
+
+
+</script>
